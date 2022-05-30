7ff013be84ee39da3ade42be493028b3558fe2f8
kanghangyeol <105232385+kanghangyeol@users.noreply.github.com>
2022-05-30 19:37:10 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+
+    scanf("%d",&a);
+    for(int i=0;i<a;i++)
+    {
+        for(int p = 0;p<i;p++)
+        {
+            printf(" ");
+        }
+        for(int q = 2*a-1;q>2*i;q--)
+        {
+            printf("*");
+        }
+        
+        
+        printf("\n");
+    }
+}
