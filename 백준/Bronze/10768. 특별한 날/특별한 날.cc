3f94729aab193f8460a8c657c4c2b8527e19ea16
kanghangyeol <105232385+kanghangyeol@users.noreply.github.com>
2022-05-31 05:21:29 +0900

    [Bronze IV] Title: 특별한 날, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.cc
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+    int m,d;
+
+    scanf("%d",&m);
+    scanf("%d",&d);
+
+    if(m==2)
+    {
+        if(d==18)
+        printf("Special");
+        else if(d<18)
+        printf("Before");
+        else
+        printf("After");
+    }
+
+    else if(m<2)
+    printf("Before");
+
+    else
+    printf("After");
+}
