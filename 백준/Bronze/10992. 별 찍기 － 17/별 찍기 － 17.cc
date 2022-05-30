a56d6e3e1a715a9673f2d927dd50c3325e48b78d
kanghangyeol <105232385+kanghangyeol@users.noreply.github.com>
2022-05-31 05:47:53 +0900

    [Bronze III] Title: 별 찍기 - 17, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.cc
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+
+    for(int i=0;i<a-1;i++)
+    {
+        for(int j=a-1;j>i;j--)
+        {
+        printf(" ");
+        }
+        printf("*");
+        for(int j=1;j<2*i;j++)
+        {
+        printf(" ");
+        }
+        if(i!=0)
+            printf("*");
+        printf("\n");
+    }
+    for(int i=0;i<2*a-1;i++)
+        printf("*");
+}
